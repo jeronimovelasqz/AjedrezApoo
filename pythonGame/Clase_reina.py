afceefe135043a4e65e5b3fbb78465ce2dc6c82c
jeronimovelasqz <103902591+jeronimovelasqz@users.noreply.github.com>
2023-03-25 18:21:45 -0500

    reina

--- pythonGame/Clase_reina.py
+++ pythonGame/Clase_reina.py
@@ -2,4 +2,28 @@ from Clase_pieza import Pieza
 
 
 class Reina(Pieza):
-    pass
+    def __init__(self, negro):
+        Pieza.__init__(self, negro)
+
+        if self.negro:
+            self.simbolo = "simbolo"
+        else:
+            self.simbolo = "simbolo"
+
+    def validar_movimiento(self, columna_inicial, fila_inicial, ultima_columna, ultima_fila, estructura_tablero):
+
+        jaque_final_valido = False
+
+        if columna_inicial == ultima_columna and fila_inicial == ultima_fila:
+            return jaque_final_valido
+
+        if Pieza.movimiento_linea_recta(self, columna_inicial, fila_inicial, ultima_columna, ultima_fila, estructura_tablero):
+            return jaque_final_valido
+
+        if Pieza.movimiento_diagonal(self, columna_inicial, fila_inicial, ultima_columna, ultima_fila, estructura_tablero):
+            return True
+
+        if Pieza.movimiento_diagonal(self, columna_inicial, fila_inicial, ultima_columna, ultima_fila, estructura_tablero):
+            return True
+
+        return jaque_final_valido
