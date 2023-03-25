cb2f3efb426c719004ff97beb9f022fad246d446
jeronimovelasqz <103902591+jeronimovelasqz@users.noreply.github.com>
2023-03-25 18:13:13 -0500

    caballo

--- pythonGame/Clase_caballo.py
+++ pythonGame/Clase_caballo.py
@@ -2,4 +2,22 @@ from Clase_pieza import Pieza
 
 
 class Caballo(Pieza):
-    pass
+
+    def __init__(self, negro):
+        Pieza.__init__(self, negro)
+        if self.negro:
+            self.simbolo = "simbolo"
+
+        else:
+            self.simbolo = "simbolo"
+
+    def validar_movimiento(self, columna_inicial, fila_inicial, ultima_columna, ultima_fila, estructura_tablero):
+
+        if abs(columna_inicial - ultima_columna) == 1 and abs(fila_inicial - ultima_fila) == 2:
+            if estructura_tablero[columna_inicial][fila_inicial].negro != estructura_tablero[ultima_columna][ultima_fila].negro:
+                return True
+
+            if abs(columna_inicial - ultima_columna) == 2 and abs(fila_inicial - ultima_fila) == 1:
+                if estructura_tablero[columna_inicial][fila_inicial].negro != estructura_tablero[ultima_columna][ultima_fila].negro:
+                    return True
+            return False
